3dc421517444a5efef435800a6b17bcbf2991692
Farkhan Azmi <ghost@Farkhans-MacBook-Pro.local>
2022-05-11 14:34:56 +0700

    fix typo name folder

--- src/stores/index.js
+++ src/stores/index.js
@@ -4,7 +4,7 @@ import thunkMiddleware from 'redux-thunk';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {global} from '@reducers';
 import {configureStore} from '@reduxjs/toolkit';
-import {loggerMiddleware} from '@middlewares';
+import {loggerMiddleware} from '@middleware';
 const globalPersistConfig = {
   key: 'global',
   storage: AsyncStorage,
